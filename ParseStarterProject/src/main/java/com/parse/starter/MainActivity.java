32469d56a16851eb972ffa0e6edc6bbf75bd20fa
Grzegorz Sztuczyński <sztuczynski.grzegorz@gmail.com>
2020-06-20 19:11:43 +0200

    Hide keyboard when user click on backgroud
    - Checkmark will jump from first to last input

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -11,9 +11,13 @@ package com.parse.starter;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -24,10 +28,22 @@ import com.parse.ParseUser;
 import com.parse.SignUpCallback;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, View.OnKeyListener {
 
     Boolean signUpModeActive = true;
     TextView loginTextView;
+    EditText usernameEditText;
+    EditText passwordEditText;
+
+    @Override
+    public boolean onKey(View view, int i, KeyEvent keyEvent) {
+
+        if (i == KeyEvent.KEYCODE_ENTER && keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
+            signUpClicked(view);
+        }
+
+        return false;
+    }
 
     @Override
     public void onClick(View view) {
@@ -45,12 +61,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 loginTextView.setText("or, Login");
             }
 
+        } else if (view.getId() == R.id.logoImageView || view.getId() == R.id.backgroundLayout) {
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
         }
     }
 
     public void signUpClicked(View view) {
-        EditText usernameEditText = findViewById(R.id.usernameEditText);
-        EditText passwordEditText = findViewById(R.id.passwordEditText);
 
         if (usernameEditText.getText().toString().matches("") || passwordEditText.getText().toString().matches("")) {
             Toast.makeText(this, "A username and a password are required.", Toast.LENGTH_SHORT).show();
@@ -91,9 +108,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         loginTextView = findViewById(R.id.loginTextView);
         loginTextView.setOnClickListener(this);
+        usernameEditText = findViewById(R.id.usernameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        ImageView logoImageView = findViewById(R.id.logoImageView);
+        RelativeLayout backgroundLayout = findViewById(R.id.backgroundLayout);
+        logoImageView.setOnClickListener(this);
+        backgroundLayout.setOnClickListener(this);
+
+        passwordEditText.setOnKeyListener(this);
 
         ParseAnalytics.trackAppOpenedInBackground(getIntent());
     }
